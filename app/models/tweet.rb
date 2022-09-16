0404fefd40ea68feb36d092e2b5f3f9ba53a7ee6
Benito Serna <bhserna@gmail.com>
2022-09-16 12:55:33 -0500

    Add broadcast methods

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,19 @@
 class Tweet < ApplicationRecord
-  after_create_commit -> { broadcast_render_later_to :tweets, partial: "tweets/on_create" }
-  after_update_commit -> { broadcast_replace_later_to :tweets }
-  after_destroy_commit -> { broadcast_render_to :tweets, partial: "tweets/on_destroy" }
+  include Broadcasts
+  
+  after_create_commit :broadcast_on_create
+  after_update_commit :broadcast_on_upate
+  after_destroy_commit :broadcast_on_destroy
+
+  def broadcast_on_create
+    broadcast_render_later_to :tweets, partial: "tweets/on_create"
+  end
+
+  def broadcast_on_upate
+    broadcast_replace_later_to :tweets
+  end
+
+  def breodcast_on_destroy
+    broadcast_render_to :tweets, partial: "tweets/on_destroy"
+  end
 end
