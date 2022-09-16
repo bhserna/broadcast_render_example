9b0c69e367b8261c95bd25b0319f263945b5031f
Benito Serna <bhserna@gmail.com>
2022-09-16 12:58:47 -0500

    Broadcasts module

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,19 +1,3 @@
 class Tweet < ApplicationRecord
   include Broadcasts
-  
-  after_create_commit :broadcast_on_create
-  after_update_commit :broadcast_on_upate
-  after_destroy_commit :broadcast_on_destroy
-
-  def broadcast_on_create
-    broadcast_render_later_to :tweets, partial: "tweets/on_create"
-  end
-
-  def broadcast_on_upate
-    broadcast_replace_later_to :tweets
-  end
-
-  def breodcast_on_destroy
-    broadcast_render_to :tweets, partial: "tweets/on_destroy"
-  end
 end
